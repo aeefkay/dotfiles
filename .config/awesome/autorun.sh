631015c89d041951bbe69263bcb37090a25f39dc
aeefkay <109334944+aeefkay@users.noreply.github.com>
2022-08-22 16:05:47 -0700

    Add files via upload

---
+++ .config/awesome/autorun.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+run() {
+  if ! pgrep -f "$1" ;
+  then
+    "$@"&
+  fi
+}
+run nitrogen --restore &
+run lxsession &
+run volumeicon &
+run picom -f &
+run ~/.screenlayout/main.sh &
+
