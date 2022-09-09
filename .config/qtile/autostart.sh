92c4a4bb385287df4bbf79d6b601a557c678f237
aeefkay <109334944+aeefkay@users.noreply.github.com>
2022-09-09 03:03:58 -0700

    Update autostart.sh

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,14 +1,7 @@
 #!/bin/sh
 # Screen layout
 xrandr --output DP-2 --primary --mode 3440x1440 --rate 100 --pos 0x413 --rotate normal --output DP-0 --mode 1920x1080 --pos 3440x0 --rotate right &
-
-
-
-# picom & disown # --experimental-backends --vsync should prevent screen tearing on most setups if needed
-
+picom & disown # --experimental-backends --vsync should prevent screen tearing on most setups if needed
 # Low battery notifier
 ~/.config/qtile/scripts/check_battery.sh & disown
-
-# Start welcome
-
-# /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 & disown # start polkit agent from GNOME
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 & disown # start polkit agent from GNOME
