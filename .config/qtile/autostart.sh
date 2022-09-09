0891cde68d9bf060a380fa7441f730bd769188cd
aeefkay <109334944+aeefkay@users.noreply.github.com>
2022-09-09 03:04:34 -0700

    Update autostart.sh

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # Screen layout
+feh --no-fehbg --bg-fill /home/afk/wallpaper/21-9/26.png' '/home/afk/wallpaper/9-16/29.png &
 xrandr --output DP-2 --primary --mode 3440x1440 --rate 100 --pos 0x413 --rotate normal --output DP-0 --mode 1920x1080 --pos 3440x0 --rotate right &
 picom & disown # --experimental-backends --vsync should prevent screen tearing on most setups if needed
 # Low battery notifier
